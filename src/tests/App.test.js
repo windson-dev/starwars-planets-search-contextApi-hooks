51892a7e579e2a98e2237d22d9df30a4e75f55a0
Windson Macedo <windsondev@hotmail.com>
2022-09-22 22:07:50 -0300

    requisito 5

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,9 +1,36 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import mockData from './mockdata';
 import App from '../App';
 
-test('I am your test', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Hello, App!/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Teste do mecanismo de busca com numeros.', () => {
+  beforeEach(() => {
+    jest.spyOn(global, 'fetch').mockImplementation(() => ({
+      json: jest.fn().mockResolvedValue(mockData),
+    }));
+  });
+
+  it('Testa se a pesquisa por numero funciona corretamente.', async () => {
+    render(<App />);
+  
+      const column = screen.getByTestId('column-filter');
+      expect(column).toBeInTheDocument();
+      userEvent.selectOptions(column, 'rotation_period');
+
+      const comparison = screen.getByTestId('comparison-filter');
+      expect(comparison).toBeInTheDocument();
+      userEvent.selectOptions(comparison, 'igual a');
+
+      const value = screen.getByTestId('value-filter');
+      expect(value).toBeInTheDocument();
+      userEvent.type(value, '23');
+
+      const button = screen.getByTestId('button-filter');
+      expect(button).toBeInTheDocument();
+      userEvent.click(button);
+
+      const result = await screen.findByText(/Hoth/i);
+      expect(result).toBeInTheDocument();
+    });
 });
